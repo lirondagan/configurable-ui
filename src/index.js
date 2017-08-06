76bd1fe2ae5188031d8d95c371dae56890c3eb48
Shulamyt <shulamyt@gmail.com>
2017-08-06 20:30:28 +0300

    react is working without hot loading

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const render = function(){
+	return ReactDOM.render(
+		<div>shula</div>,
+		document.getElementById('main')
+	);
+};
+
+render();
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,14 @@ module.exports = {
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				use: ['babel-loader'],
+				exclude: /node_modules/
+			},
+		]
 	}
 };
