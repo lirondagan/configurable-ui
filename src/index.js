4504cf83377113a78c75518938530cef4b1f9638
Shulamyt <shulamyt@gmail.com>
2017-08-07 13:00:38 +0300

    strat to config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const render = function(){
+const render = function(config){
+	let rootElement = createElementsFromConfig(config);
+
 	return ReactDOM.render(
-		<div>shula</div>,
+		rootElement,
 		document.getElementById('main')
 	);
+
+	// return ReactDOM.render(
+	// 	<div>hhh</div>,
+	// 	document.getElementById('main')
+	// );
 };
 
-render();
+const getChildren = function(config){
+	return null;
+};
+
+const getElementType = function(config){
+	return config.element;
+};
+
+const getProps = function(config){
+	return null;
+};
+
+const createElementsFromConfig = function(config){
+	// Object.keys(config).map(function(elementKey){
+	// 	let elementConfig = config[elementKey];
+	// 	createElementFromConfig(elementConfig);
+	// });
+	let elementKey = 'name';
+	let elementConfig = config[elementKey];
+	elementConfig.key = elementKey;
+	return createElementFromConfig(elementConfig);
+};
+
+const createElementFromConfig = function(config){
+	let elementType = getElementType(config);
+	let props = getProps(config);
+	let children = getChildren(config);
+	
+	let element = React.createElement(elementType, props, children);
+	return element;
+};
+
+var config = {
+	name: {
+		element: 'input',
+		field: 'name',
+		type: 'text',
+		value: 'myValue'
+	},
+	// personalDetailsGroup : {
+	// 	element: 'div',
+	// 	className: 'personalDetailsGroup',
+	// 	children: {
+	// 		firstName: {
+	// 			element: 'firstName',
+	// 			field: 'firstName',
+	// 			type: 'text',
+	// 			value: 'Shula'
+	// 		},
+	// 		lastName: {
+	// 			element: 'lastName',
+	// 			field: 'lastName',
+	// 			type: 'text',
+	// 			value: 'Ajami'
+	// 		},
+	// 	}
+	// }
+};
+
+render(config);
